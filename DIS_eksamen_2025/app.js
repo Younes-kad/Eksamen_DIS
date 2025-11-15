68cd81efd92b118b92f72cb347b18594286f5e94
Younes-kad <vpkadimmer@gmail.com>
2025-11-15 01:16:38 +0100

    udkast

--- DIS_eksamen_2025/app.js
+++ DIS_eksamen_2025/app.js
@@ -5,13 +5,13 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
 
 var app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'pug');
+// We serve plain HTML files (not using Pug), so remove view engine to avoid attempts to render Pug templates.
+
 
 app.use(logger('dev'));
 app.use(express.json());
@@ -20,7 +20,6 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
@@ -29,13 +28,23 @@ app.use(function(req, res, next) {
 
 // error handler
 app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
+  // send a simple HTML error response (we're not using Pug)
+  var status = err.status || 500;
+  res.status(status);
+  if (req.app.get('env') === 'development') {
+    res.send('<h1>Error ' + status + '</h1><p>' + (err.message || '') + '</p><pre>' + (err.stack || '') + '</pre>');
+  } else {
+    res.send('<h1>Error ' + status + '</h1><p>Something went wrong.</p>');
+  }
 });
 
 module.exports = app;
+
+// If this file is run directly, start the server (so `node app.js` works)
+if (require.main === module) {
+  var port = process.env.PORT || 3001;
+  app.set('port', port);
+  app.listen(port, function() {
+    console.log('Server listening on http://localhost:' + port);
+  });
+}
