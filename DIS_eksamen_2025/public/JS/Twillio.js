e1b8a0c4fadaea206fe7a0b0d91cc0d68b7e6324
Issahbhai <issahrana@gmail.com>
2025-11-22 13:41:55 +0100

    sv

--- DIS_eksamen_2025/public/JS/Twillio.js
+++ DIS_eksamen_2025/public/JS/Twillio.js
@@ -4,10 +4,6 @@ const accountSid = process.env.TWILIO_ACCOUNT_SID;
 const authToken = process.env.TWILIO_AUTH_TOKEN;
 const fromNumber = process.env.TWILIO_PHONE_NUMBER;
 
-if (!accountSid || !authToken || !fromNumber) {
-  throw new Error('Twilio miljøvariabler mangler (TWILIO_ACCOUNT_SID, TWILIO_AUTH_TOKEN, TWILIO_PHONE_NUMBER)');
-}
-
 const client = twilio(accountSid, authToken);
 
 async function sendSms(to, body) {
